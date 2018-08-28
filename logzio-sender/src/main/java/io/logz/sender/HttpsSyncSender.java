9f574741dc411d608aa4afbd5c9f3d2f1e45e5cd
idohalevi <ido.h@logz.io>
2018-08-28 16:35:49 +0300

    optimizing imports and editing README

--- logzio-sender/src/main/java/io/logz/sender/HttpsSyncSender.java
+++ logzio-sender/src/main/java/io/logz/sender/HttpsSyncSender.java
@@ -5,12 +5,10 @@ import io.logz.sender.exceptions.LogzioServerErrorException;
 import java.io.BufferedReader;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.InputStream;
 import java.io.InputStreamReader;
-
+import java.io.OutputStream;
 import java.net.HttpURLConnection;
-
 import java.util.List;
 import java.util.zip.GZIPOutputStream;
 
