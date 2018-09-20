83b9edc5c7316c85d95783e7c6362e86d3b2bf09
idohalevi <ido.h@logz.io>
2018-09-20 14:51:54 +0300

    refactoring tests - working with builder instead of sender object

--- logzio-sender/src/test/java/io/logz/sender/LogzioLongRunningTests.java
+++ logzio-sender/src/test/java/io/logz/sender/LogzioLongRunningTests.java
@@ -140,7 +140,6 @@ public class LogzioLongRunningTests {
             }
 
             assertThat(threadIds).isNull();
-
         } finally {
             threads.forEach(Thread::interrupt);
         }
